6feb76693099e3d29edd9b27088cd854a6180f65
tyzual <e.tyzual@gmail.com>
2017-07-20 17:21:07 +0800

    修复bug

--- tserverlogic/server.go
+++ tserverlogic/server.go
@@ -11,29 +11,25 @@ import (
 HandleAddComment 添加评论Handler
 */
 func HandleAddComment(w http.ResponseWriter, r *http.Request) {
-	go func() {
-		defer r.Body.Close()
-		if r.Method != http.MethodPost {
-			tutil.Log(fmt.Sprintf("request from:%v", r.Host))
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("ERROR: USE POST"))
-			return
-		}
-		tutil.Log(fmt.Sprintf("%v", r.Method))
-	}()
+	defer r.Body.Close()
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		tutil.Log(fmt.Sprintf("request from:%v", r.Host))
+		w.Write([]byte("ERROR: USE POST"))
+		return
+	}
+	tutil.Log(fmt.Sprintf("%v", r.Method))
 }
 
 /*
 HandleCommentList 获取评论列表Handler
 */
 func HandleCommentList(w http.ResponseWriter, r *http.Request) {
-	go func() {
-		defer r.Body.Close()
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("ERROR: USE POST"))
-			return
-		}
-		tutil.Log(fmt.Sprintf("%v", r.Method))
-	}()
+	defer r.Body.Close()
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("ERROR: USE POST"))
+		return
+	}
+	tutil.Log(fmt.Sprintf("%v", r.Method))
 }
